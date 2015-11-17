cc113482eec0ae5ef3881e5749de7c4ff3ef90a9
Jialiang Song <jialiang@jialiangs-mbp.acs.ocad.ca>
2015-11-17 00:17:33 -0500

    js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('button').on('click',function(e){
+
+$('.modal').fadeIn();
+
+});
+
+$('.close').on('click',function(e){
+
+$('.modal').fadeOut();
+});
+
+$('.submit').on('click',function(e){
+
+	$('input').addClass('error');
+
+});
+
+$('input').on('mouseover',function(e){
+
+	$(this).removeClass('error')
+});
+
+$('.modal').on('click',function(e){
+
+	$(this).fadeOut();
+});
